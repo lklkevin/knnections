d63f7fa55fb1aa800b1f9e9c40ceeb7788e1a947
lklkevin <88564778+lklkevin@users.noreply.github.com>
2024-07-25 19:14:55 -0400

    improved output formatting

--- scraper.py
+++ scraper.py
@@ -6,6 +6,7 @@ url = 'https://tryhardguides.com/nyt-connections-answers/'
 headers = {'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Mobile Safari/537.36'}
 response = requests.get(url, headers=headers)
 # print(response.content.decode())
+cols = {0: 'yellow', 1: 'green', 2: 'blue', 3: 'purple'}
 
 if response.status_code == 200:
     web_content = response.content
@@ -14,17 +15,26 @@ if response.status_code == 200:
     connections = {}
 
     outer_li_tags = soup.find_all('li')
+    day = 0
 
     for outer_li in outer_li_tags:
         nested_ul = outer_li.find('ul')
         if nested_ul:
             nested_li_tags = nested_ul.find_all('li')
+            colours = {}
+            difficulty = 0
+
             for nested_li in nested_li_tags:
                 category_tag = nested_li.find('strong')
                 if category_tag:
                     category = category_tag.get_text()
                     words = nested_li.get_text().replace(category, '').replace('-', '').strip()
-                    connections[category] = [word.strip() for word in words.split(',')]
+                    category = ' '.join([c for c in category.split() if c.isalpha()])
+                    colours[cols[difficulty]] = {category: [word.strip() for word in words.split(',') if word.strip().isalpha()]}
+                    difficulty += 1
+
+            connections[day] = colours
+            day += 1
 
     with open('data.json', 'w') as file:
         json.dump(connections, file, indent=4)
