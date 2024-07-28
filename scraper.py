c4731efe67b699ad681d79c88bdf6fe275a82950
likevi53 <lkevin.li@mail.utoronto.ca>
2024-07-28 04:09:11 -0400

    naive kmeans guesser

--- scraper.py
+++ scraper.py
@@ -3,48 +3,77 @@ from bs4 import BeautifulSoup
 import json
 
 
+def get_data():
+    connections = scrape()
+    data = []
+    for k in connections.keys():
+        data.append((extract(connections[k])))
+
+    return data
+
+
 def clean(word: str) -> str:
     lst = list(word)
     new = [n for n in lst if n.isalnum() or n.isspace()]
 
     return ''.join(new)
 
-url = 'https://tryhardguides.com/nyt-connections-answers/'
-headers = {'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Mobile Safari/537.36'}
-response = requests.get(url, headers=headers)
-# print(response.content.decode())
-cols = {0: 'yellow', 1: 'green', 2: 'blue', 3: 'purple'}
 
-if response.status_code == 200:
-    web_content = response.content
+def extract(inp: dict) -> tuple:
+    cats = []
+    wrds = []
+    for key in inp:
+        cat = [k for k in inp[key].keys()][0]
+        cats.append(cat)
+        wrds.extend(inp[key][cat])
+
+    return cats, wrds
+
+
+def scrape():
+    url = 'https://tryhardguides.com/nyt-connections-answers/'
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Mobile Safari/537.36'}
+    response = requests.get(url, headers=headers)
+    # print(response.content.decode())
+    cols = {0: 'yellow', 1: 'green', 2: 'blue', 3: 'purple'}
+
+    if response.status_code == 200:
+        web_content = response.content
+
+        soup = BeautifulSoup(web_content, 'html.parser')
+        connections = {}
+
+        outer_li_tags = soup.find_all('li')
+        day = 0
+
+        for outer_li in outer_li_tags:
+            nested_ul = outer_li.find('ul')
+            if nested_ul:
+                nested_li_tags = nested_ul.find_all('li')
+                colours = {}
+                difficulty = 0
+
+                for nested_li in nested_li_tags:
+                    category_tag = nested_li.find('strong')
+                    if category_tag:
+                        category = category_tag.get_text()
+                        words = nested_li.get_text().replace(category, '', 1).replace('-', '').strip()
+                        temp = [clean(c.strip()) for c in category.split()]
+                        category = ' '.join([t for t in temp if t]).strip()
+                        colours[cols[difficulty]] = {category: [clean(word.strip()) for word in words.split(',')]}
+                        difficulty += 1
+
+                connections[day] = colours
+                day += 1
+        
+        return connections
+    else:
+        print(f"Failed to retrieve the web page. Status code: {response.status_code}")
+        return {}
     
-    soup = BeautifulSoup(web_content, 'html.parser')
-    connections = {}
 
-    outer_li_tags = soup.find_all('li')
-    day = 0
-
-    for outer_li in outer_li_tags:
-        nested_ul = outer_li.find('ul')
-        if nested_ul:
-            nested_li_tags = nested_ul.find_all('li')
-            colours = {}
-            difficulty = 0
-
-            for nested_li in nested_li_tags:
-                category_tag = nested_li.find('strong')
-                if category_tag:
-                    category = category_tag.get_text()
-                    words = nested_li.get_text().replace(category, '', 1).replace('-', '').strip()
-                    temp = [clean(c.strip()) for c in category.split()]
-                    category = ' '.join([t for t in temp if t]).strip()
-                    colours[cols[difficulty]] = {category: [clean(word.strip()) for word in words.split(',')]}
-                    difficulty += 1
-
-            connections[day] = colours
-            day += 1
-
-    with open('data.json', 'w') as file:
+def export(name: str):
+    connections = scrape()
+    with open(name, 'w') as file:
         json.dump(connections, file, indent=4)
-else:
-    print(f"Failed to retrieve the web page. Status code: {response.status_code}")
--- vectorize.py
+++ vectorize.py
@@ -1,48 +1,113 @@
+import random
+
 import gensim.downloader as api
 import numpy as np
 from sklearn.cluster import KMeans
 from k_means_constrained import KMeansConstrained
-import matplotlib.pyplot as plt
+import scraper
 
-words = ["CLAY",
-         "PAPYRUS",
-         "PARCHMENT",
-         "WAX",
-         "ANCHOR",
-         "HOST",
-         "MODERATE",
-         "PRESENT",
-         "FACULTY",
-         "FLAIR",
-         "INSTINCT",
-         "TALENT",
-         "BURRITO",
-         "GIFT",
-         "MUMMY",
-         "SPRAIN"
-         ]
 
-vectorizer = api.load('glove-twitter-25')
-word_vectors = []
+def init_vectors(vect_name: str):
+    data = scraper.get_data()
+    vectorizer = api.load(vect_name)
 
-for word in words:
-    word = word.lower()
-    if word in vectorizer:
-        word_vectors.append(vectorizer[word])
+    features = []
+    labels = []
+    correct = []
 
-word_vectors = np.array(word_vectors)
-km = KMeansConstrained(n_clusters=4, size_min=4, size_max=4, random_state=0)
-fitted = km.fit(word_vectors)
+    for entry in data:
+        x = entry[1]
+        y = entry[0]
+        if x == "" or y == "":
+            continue
 
-clusters = km.predict(word_vectors)
+        xk, y = vectorize(x, y, vectorizer)
+        if xk is not None and len(x) == 16:
+            features.append(xk)
+            correct.append(np.array(x))
+            labels.append(y)
 
-# Group words by their cluster
-grouped_words = {}
-for word, cluster in zip(words, clusters):
-    if cluster not in grouped_words:
-        grouped_words[cluster] = []
-    grouped_words[cluster].append(word)
+    return features, correct
 
-# Print the grouped words
-for cluster, group in grouped_words.items():
-    print(f"Cluster {cluster}: {group}")
+
+def vectorize(x: list, y: list, vectorizer):
+    word_vectors = []
+    label_vectors = []
+    for word in x:
+        word = word.lower()
+        if word in vectorizer:
+            word_vectors.append(vectorizer[word])
+        else:
+            return None, None
+
+    # for word in y:
+    #     word = word.lower()z
+    #     if word in vectorizer:
+    #         label_vectors.append(vectorizer[word])
+    #     else:
+    #         print(word)
+    #         return None, None
+
+    return np.array(word_vectors), np.array(label_vectors)
+
+
+def random_guess():
+    data = scraper.get_data()
+    total_groups = 0
+    correct_groups = 0
+
+    for entry in data:
+        correct = entry[1]
+        if len(correct) != 16:
+            continue
+
+        g1 = correct[0:4]
+        g2 = correct[4:8]
+        g3 = correct[8:12]
+        g4 = correct[12:16]
+
+        correct_groups_list = [g1, g2, g3, g4]
+
+        shuffled_correct = correct[:]
+        random.shuffle(shuffled_correct)
+
+        guessed_groups = [shuffled_correct[i:i + 4] for i in range(0, 16, 4)]
+
+        for guessed_group in guessed_groups:
+            if guessed_group in correct_groups_list:
+                correct_groups += 1
+                print(guessed_group)
+
+        total_groups += 4
+
+    return correct_groups / total_groups
+def kmeans(word_vectors, correct):
+    km = KMeansConstrained(n_clusters=4, size_min=4, size_max=5, random_state=0)
+    fitted = km.fit(word_vectors)
+    g1 = correct[0:4]
+    g2 = correct[4:8]
+    g3 = correct[8:12]
+    g4 = correct[12:16]
+
+    clusters = km.predict(word_vectors)
+    grouped_words = {}
+    for word, cluster in zip(correct, clusters):
+        if cluster not in grouped_words:
+            grouped_words[cluster] = []
+        grouped_words[cluster].append(word)
+
+    right = 0
+    for cluster, group in grouped_words.items():
+        if any(np.array_equal(group, g) for g in [g1, g2, g3, g4]):
+            print(group)
+            right += 1
+
+    return right
+
+print(random_guess())
+# a, b = init_vectors('fasttext-wiki-news-subwords-300')
+# counter = 0
+# for i in range(len(b)):
+#     counter += kmeans(a[i], b[i])
+#
+# print(counter / (len(b) * 4))
